4ddab011999396b127d24d868334303a891f0103
Soumya <soumyaric2@gmail.com>
2025-03-12 15:52:50 +0530

    HomeScreen Initialization and Bottom NavigationBar

---
+++ app/src/main/java/com/example/woocom/pages/HomePage.kt
@@ -0,0 +1,10 @@
+package com.example.woocom.pages
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomePage(modifier: Modifier=Modifier) {
+    Text(text = "Home Page")
+}
