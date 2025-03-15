503eae169a5b37509b19dc4f855487eff1750742
Soumya <soumyaric2@gmail.com>
2025-03-15 18:45:18 +0530

    Added Categories, Integration of Lazy Columns

---
+++ app/src/main/java/com/example/woocom/model/CategoryModel.kt
@@ -0,0 +1,8 @@
+package com.example.woocom.model
+
+data class CategoryModel(
+    val id : String = "",
+    val Name : String = "",
+    val imageUrl : String = ""
+
+)
--- app/src/main/java/com/example/woocom/pages/HomePage.kt
+++ app/src/main/java/com/example/woocom/pages/HomePage.kt
@@ -6,8 +6,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.woocom.components.BannerView
+import com.example.woocom.components.CategoriesView
 import com.example.woocom.components.HeaderView
 
 @Composable
@@ -17,5 +22,16 @@ fun HomePage(modifier: Modifier=Modifier) {
         .padding(16.dp)) {
         HeaderView(Modifier)
         BannerView(modifier)
+
+        Text(text = "Categories",
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
+            )
+        )
+
+        CategoriesView()
+
     }
 }
+
